da78dc0ccd7d21d3dbc9ad55b6acebefe478e5a0
emincan <emincanozcann@gmail.com>
2021-05-06 22:44:31 +0300

    design bug fix

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="w-full h-24 sticky flex items-center top-0 left-0 bg-gray-900 border-b-2 border-gray-700 mb-4 py-4"
+    class="w-full h-24 sticky flex items-center top-0 left-0 bg-gray-900 border-b-2 border-gray-700 mb-4 py-4 z-50"
   >
     <slot />
   </header>
