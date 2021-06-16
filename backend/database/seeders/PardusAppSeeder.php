c454835f810a3e1ec5987b9e976867a2d2d2b926
Emincan Ozcan <emincanozcann@gmail.com>
2021-06-16 13:11:52 +0300

    wip

--- backend/database/seeders/PardusAppSeeder.php
+++ backend/database/seeders/PardusAppSeeder.php
@@ -75,97 +75,6 @@ echo 'Tee ok.'",
 
     public function run()
     {
-        $this->setScript('Signal', [
-            "wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg",
-            "cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null",
-            "echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list",
-            "sudo apt update && sudo apt install signal-desktop -y"
-        ]);
-
-        $this->setScript('Docker', [
-            'sudo apt install docker.io -y',
-            'sudo systemctl start docker',
-            'sudo systemctl enable docker',
-            'sudo apt install curl -y',
-            'sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose',
-            'sudo chmod +x /usr/local/bin/docker-compose',
-        ]);
-        $this->setScript('ParseHub', [
-            'sudo apt install curl -y',
-            'curl -L https://parsehub.com/static/client/parsehub.tar.gz | tar -xzf - -C /tmp',
-            'sudo mv /tmp/parsehub /opt/',
-            'sudo ln -s /opt/parsehub/parsehub /usr/local/bin/',
-            'rm -f parsehub.tar.gz'
-        ]);
-
-        $this->setScript('Celestia', [
-            'sudo apt install curl -y',
-            'curl https://celestia.space/packages/celestia.key | sudo apt-key add -',
-            'echo deb https://celestia.space/packages buster main | sudo tee /etc/apt/sources.list.d/celestia.list',
-            'sudo apt install celestia -y',
-        ]);
-
-        $this->setScript('Pale Moon', [
-            'sudo apt install curl -y',
-            "echo 'deb http://download.opensuse.org/repositories/home:/stevenpusser/Debian_9.0/ /' | sudo tee /etc/apt/sources.list.d/home:stevenpusser.list",
-            'curl -fsSL https://download.opensuse.org/repositories/home:stevenpusser/Debian_9.0/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_stevenpusser.gpg > /dev/null',
-            'sudo apt update',
-            'sudo apt install palemoon'
-        ]);
-
-        $this->setScript('DBeaver', [
-            "wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | sudo apt-key add -",
-            'echo "deb https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list',
-            "sudo apt update",
-            "sudo apt -y install dbeaver-ce",
-        ]);
-
-        $this->setScript('Element', [
-            "sudo apt install -y wget apt-transport-https",
-            'sudo wget -O /usr/share/keyrings/riot-im-archive-keyring.gpg https://packages.riot.im/debian/riot-im-archive-keyring.gpg',
-            'echo "deb [signed-by=/usr/share/keyrings/riot-im-archive-keyring.gpg] https://packages.riot.im/debian/ default main" | sudo tee /etc/apt/sources.list.d/riot-im.list',
-            "sudo apt update",
-            "sudo apt install element-desktop",
-        ]);
-
-        $this->setScript('Zotero', [
-            "sudo wget -qO- https://github.com/retorquere/zotero-deb/releases/download/apt-get/install.sh | sudo bash",
-            'sudo apt update',
-            'sudo apt install zotero',
-        ]);
-
-        $this->setScript('Mozilla Firefox', ['( sudo apt install firefox-esr -y || sudo apt install firefox -y)']);
-
-        $this->appImage('Cryptomator', "https://github.com/cryptomator/cryptomator/releases/download/1.5.15/cryptomator-1.5.15-x86_64.AppImage");
-        $this->appImage('Joplin', "https://github.com/laurent22/joplin/releases/download/v1.8.5/Joplin-1.8.5.AppImage");
-
-        $this->wgetDeb('Vagrant', 'https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.deb');
-        $this->wgetDeb('Angry IP Scanner', 'https://github.com/angryip/ipscan/releases/download/3.7.6/ipscan_3.7.6_amd64.deb');
-        $this->wgetDeb('Insomnia REST Client', 'https://updates.insomnia.rest/downloads/ubuntu/latest?&app=com.insomnia.app&source=website');
-        $this->wgetDeb('Google Chrome', 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb');
-        $this->wgetDeb('GitKraken', 'https://release.gitkraken.com/linux/gitkraken-amd64.deb');
-        $this->wgetDeb('SmartGit', 'https://www.syntevo.com/downloads/smartgit/smartgit-20_2_5.deb');
-        $this->wgetDeb('4k Video Downloader', 'https://dl.4kdownload.com/app/4kvideodownloader_4.16.0-1_amd64.deb');
-        $this->wgetDeb('Tixati', 'https://download2.tixati.com/download/tixati_2.83-1_amd64.deb');
-        $this->wgetDeb('Trillian', 'https://www.trillian.im/get/linux/6.3/?deb=64');
-        $this->wgetDeb('1Password', 'https://downloads.1password.com/linux/debian/amd64/stable/1password-latest.deb');
-        $this->wgetDeb('PeaZip', 'https://github.com/peazip/PeaZip/releases/download/7.9.0/peazip_7.9.0.LINUX.x86_64.GTK2.deb');
-        $this->wgetDeb('Bitwarden', 'https://github.com/bitwarden/desktop/releases/download/v1.26.4/Bitwarden-1.26.4-amd64.deb');
-        $this->wgetDeb('VeraCrypt', 'https://launchpad.net/veracrypt/trunk/1.24-update7/+download/veracrypt-1.24-Update7-Debian-10-amd64.deb');
-        $this->wgetDeb('MEGA', 'https://mega.nz/linux/MEGAsync/Debian_10.0/amd64/megasync_4.4.0-1.1_amd64.deb');
-        $this->wgetDeb('Pencil Project', 'https://pencil.evolus.vn/dl/V3.1.0.ga/pencil_3.1.0.ga_amd64.deb');
-        $this->wgetDeb('WPS Office', 'https://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/10161/wps-office_11.1.0.10161.XA_amd64.deb');
-        $this->wgetDeb('Mattermost', 'https://releases.mattermost.com/desktop/4.6.2/mattermost-desktop-4.6.2-linux-amd64.deb');
-        $this->wgetDeb('ManicTime', 'https://cdn.manictime.com/setup/linux/v1_3_5_0/ManicTime.deb');
-        $this->wgetDeb('balenaEtcher', 'https://github.com/balena-io/etcher/releases/download/v1.5.120/balena-etcher-electron_1.5.120_amd64.deb');
-        $this->wgetDeb('Obsidian', 'https://github.com/obsidianmd/obsidian-releases/releases/download/v0.9.20/obsidian_0.9.20_amd64.deb');
-        $this->makeInstallationSame('WPS Office', 'WPS Writer');
-        $this->wgetDeb('Steam', 'https://media.steampowered.com/client/installer/steam.deb', [
-            "sudo dpkg --add-architecture i386",
-            'sudo apt update',
-            "sudo apt install libgl1-mesa-glx:i386 -y",
-        ]);
-
 
         $this->ondokuzon('Spotify', 'spotify-client');
         $this->ondokuzon('Audacity', 'audacity');
@@ -188,7 +97,6 @@ echo 'Tee ok.'",
         $this->ondokuzon('Thunderbird', 'thunderbird');
         $this->ondokuzon('Kdenlive', 'kdenlive');
         $this->ondokuzon('OpenShot', 'openshot-qt');
-//        $this->$this->ondokuzon('Python', 'python3');
         $this->ondokuzon('Dropbox', 'dropbox');
         $this->ondokuzon('TeamViewer', 'teamviewer');
         $this->ondokuzon('VirtualBox', 'virtualbox6.1');
@@ -242,7 +150,6 @@ echo 'Tee ok.'",
         $this->ondokuzon('MusicBrainz Picard', 'picard');
         $this->ondokuzon('Scribus', 'scribus');
         $this->ondokuzon('TestDisk', 'testdisk');
-//        $this->ondokuzon('Tiny Tiny RSS', 'tt-rss');
         $this->ondokuzon('AnyDesk', 'anydesk');
         $this->ondokuzon('Viber', 'viber');
         $this->ondokuzon('Stellarium', 'stellarium');
@@ -327,6 +234,39 @@ echo 'Tee ok.'",
         $this->ondokuzon('LMMS', 'lmms');
         $this->ondokuzon('htop', 'htop');
 
+
+        $this->appImage('Cryptomator', "https://github.com/cryptomator/cryptomator/releases/download/1.5.15/cryptomator-1.5.15-x86_64.AppImage");
+        $this->appImage('Joplin', "https://github.com/laurent22/joplin/releases/download/v1.8.5/Joplin-1.8.5.AppImage");
+
+
+        $this->wgetDeb('Vagrant', 'https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.deb');
+        $this->wgetDeb('Angry IP Scanner', 'https://github.com/angryip/ipscan/releases/download/3.7.6/ipscan_3.7.6_amd64.deb');
+        $this->wgetDeb('Insomnia REST Client', 'https://updates.insomnia.rest/downloads/ubuntu/latest?&app=com.insomnia.app&source=website');
+        $this->wgetDeb('Google Chrome', 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb');
+        $this->wgetDeb('GitKraken', 'https://release.gitkraken.com/linux/gitkraken-amd64.deb');
+        $this->wgetDeb('SmartGit', 'https://www.syntevo.com/downloads/smartgit/smartgit-20_2_5.deb');
+        $this->wgetDeb('4k Video Downloader', 'https://dl.4kdownload.com/app/4kvideodownloader_4.16.0-1_amd64.deb');
+        $this->wgetDeb('Tixati', 'https://download2.tixati.com/download/tixati_2.83-1_amd64.deb');
+        $this->wgetDeb('Trillian', 'https://www.trillian.im/get/linux/6.3/?deb=64');
+        $this->wgetDeb('1Password', 'https://downloads.1password.com/linux/debian/amd64/stable/1password-latest.deb');
+        $this->wgetDeb('PeaZip', 'https://github.com/peazip/PeaZip/releases/download/7.9.0/peazip_7.9.0.LINUX.x86_64.GTK2.deb');
+        $this->wgetDeb('Bitwarden', 'https://github.com/bitwarden/desktop/releases/download/v1.26.4/Bitwarden-1.26.4-amd64.deb');
+        $this->wgetDeb('VeraCrypt', 'https://launchpad.net/veracrypt/trunk/1.24-update7/+download/veracrypt-1.24-Update7-Debian-10-amd64.deb');
+        $this->wgetDeb('MEGA', 'https://mega.nz/linux/MEGAsync/Debian_10.0/amd64/megasync_4.4.0-1.1_amd64.deb');
+        $this->wgetDeb('Pencil Project', 'https://pencil.evolus.vn/dl/V3.1.0.ga/pencil_3.1.0.ga_amd64.deb');
+        $this->wgetDeb('WPS Office', 'https://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/10161/wps-office_11.1.0.10161.XA_amd64.deb');
+        $this->wgetDeb('Mattermost', 'https://releases.mattermost.com/desktop/4.6.2/mattermost-desktop-4.6.2-linux-amd64.deb');
+        $this->wgetDeb('ManicTime', 'https://cdn.manictime.com/setup/linux/v1_3_5_0/ManicTime.deb');
+        $this->wgetDeb('balenaEtcher', 'https://github.com/balena-io/etcher/releases/download/v1.5.120/balena-etcher-electron_1.5.120_amd64.deb');
+        $this->wgetDeb('Obsidian', 'https://github.com/obsidianmd/obsidian-releases/releases/download/v0.9.20/obsidian_0.9.20_amd64.deb');
+        $this->makeInstallationSame('WPS Office', 'WPS Writer');
+        $this->wgetDeb('Steam', 'https://media.steampowered.com/client/installer/steam.deb', [
+            "sudo dpkg --add-architecture i386",
+            'sudo apt update',
+            "sudo apt install libgl1-mesa-glx:i386 -y",
+        ]);
+
+
         $this->snap('Shotcut', 'shotcut');
         $this->snap('Typora', 'typora');
         $this->snap('WebStorm', 'webstorm');
@@ -339,6 +279,68 @@ echo 'Tee ok.'",
         $this->snap('Standard Notes', 'standard-notes');
         $this->snap('ONLYOFFICE', 'onlyoffice-desktopeditors');
         $this->snap('RetroArch', 'retroarch');
+
+
+        $this->setScript('Signal', [
+            "wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg",
+            "cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null",
+            "echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list",
+            "sudo apt update && sudo apt install signal-desktop -y"
+        ]);
+
+        $this->setScript('Docker', [
+            'sudo apt install docker.io -y',
+            'sudo systemctl start docker',
+            'sudo systemctl enable docker',
+            'sudo apt install curl -y',
+            'sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose',
+            'sudo chmod +x /usr/local/bin/docker-compose',
+        ]);
+        $this->setScript('ParseHub', [
+            'sudo apt install curl -y',
+            'curl -L https://parsehub.com/static/client/parsehub.tar.gz | tar -xzf - -C /tmp',
+            'sudo mv /tmp/parsehub /opt/',
+            'sudo ln -s /opt/parsehub/parsehub /usr/local/bin/',
+            'rm -f parsehub.tar.gz'
+        ]);
+
+        $this->setScript('Celestia', [
+            'sudo apt install curl -y',
+            'curl https://celestia.space/packages/celestia.key | sudo apt-key add -',
+            'echo deb https://celestia.space/packages buster main | sudo tee /etc/apt/sources.list.d/celestia.list',
+            'sudo apt install celestia -y',
+        ]);
+
+        $this->setScript('Pale Moon', [
+            'sudo apt install curl -y',
+            "echo 'deb http://download.opensuse.org/repositories/home:/stevenpusser/Debian_9.0/ /' | sudo tee /etc/apt/sources.list.d/home:stevenpusser.list",
+            'curl -fsSL https://download.opensuse.org/repositories/home:stevenpusser/Debian_9.0/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_stevenpusser.gpg > /dev/null',
+            'sudo apt update',
+            'sudo apt install palemoon'
+        ]);
+
+        $this->setScript('DBeaver', [
+            "wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | sudo apt-key add -",
+            'echo "deb https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list',
+            "sudo apt update",
+            "sudo apt -y install dbeaver-ce",
+        ]);
+
+        $this->setScript('Element', [
+            "sudo apt install -y wget apt-transport-https",
+            'sudo wget -O /usr/share/keyrings/riot-im-archive-keyring.gpg https://packages.riot.im/debian/riot-im-archive-keyring.gpg',
+            'echo "deb [signed-by=/usr/share/keyrings/riot-im-archive-keyring.gpg] https://packages.riot.im/debian/ default main" | sudo tee /etc/apt/sources.list.d/riot-im.list',
+            "sudo apt update",
+            "sudo apt install element-desktop",
+        ]);
+
+        $this->setScript('Zotero', [
+            "sudo wget -qO- https://github.com/retorquere/zotero-deb/releases/download/apt-get/install.sh | sudo bash",
+            'sudo apt update',
+            'sudo apt install zotero',
+        ]);
+
+        $this->setScript('Mozilla Firefox', ['( sudo apt install firefox-esr -y || sudo apt install firefox -y)']);
 
 //        [ "C++", "C (programming language)", "C#","PHP", "Go (Programming Language)",  ]
     }
